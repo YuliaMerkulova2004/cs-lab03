272e81cf94421957e45fe391a7ba3077cc65deea
MerkulovaYD <kurskyulia@mail.ru>
2024-06-30 22:17:32 +0300

    Использование SVG в варианте №7

--- main.cpp
+++ main.cpp
@@ -125,25 +125,49 @@ void svg_text(double left, double baseline, string text) {
     cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
 }
 
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
+void svg_rect(double x, double y, double width, double height, string stroke = "green", string fill = "green") {
     cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
 }
 
 void show_histogram_svg(const vector<size_t>& bins) {
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
+    const auto IMAGE_WIDTH = 1000;
+    const auto IMAGE_HEIGHT = 200;
     const auto TEXT_LEFT = 20;
     const auto TEXT_BASELINE = 20;
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+
+    const size_t MAX_LEN = 76;
+    size_t number_count = accumulate(bins.begin(), bins.end(), 0);
+    size_t bin_count = bins.size();
+    size_t average_height = static_cast<size_t>(round(static_cast<double>(number_count) / bin_count));
+    size_t max_value = max_count(bins);
+
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
-    for (size_t bin : bins) {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-        top += BIN_HEIGHT;
+    if (max_value > MAX_LEN) {
+        for (size_t bin : bins) {
+            const double bin_width = BLOCK_WIDTH * static_cast<double>(bin) * MAX_LEN / max_value;
+            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+            if (bin > average_height) {
+                svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
+            } else {
+                svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+            }
+            top += BIN_HEIGHT;
+        }
+    } else {
+        for (size_t bin : bins) {
+            const double bin_width = BLOCK_WIDTH * bin;
+            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+            if (bin > average_height) {
+                svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "red");
+            } else {
+                svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+            }
+            top += BIN_HEIGHT;
+        }
     }
     svg_end();
 }
